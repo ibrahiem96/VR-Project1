189e0d4b74abab1f3c81713e32a4ed0402cecb07
Ibrahiem Mohammad <ibrahiem.mohammed96@gmail.com>
2017-09-25 19:52:44 -0500

    add source code

---
+++ evl 491/evl 491/Assets/LightControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightControl : MonoBehaviour {
+    private Light light;
+	// Use this for initialization
+	void Start () {
+        light = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.L)){
+            light.enabled = !light.enabled;
+        }
+
+
+	}
+}
